09b20709f88522a5d0b57a00447a9438b0f15502
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-06-19 23:50:00 +0100

    Working on Book controller tests.

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -5,31 +5,40 @@ describe BooksController do
   before(:each) do
     request.env["HTTP_ACCEPT"] = 'application/json'
     @book = FactoryGirl.create(:book)
+    5.times { FactoryGirl.create(:book) }
   end
 
-  context 'when a user loads the books page' do
 
     describe "GET /index" do 
-      it "should return all books" do 
+      it "should respond with success" do 
         get :index
-        ap response.body 
         expect(response.status).to eq 200
       end 
     end
 
+    describe "POST /create" do
+      it "should respond with success" do
+        post :create, book: {:title => "Harry Potter and the Chamber of Open Secrets", :isbn => "#{rand(10 ** 13)}"}
+        expect(response.status).to eq 201
+      end
+      it "should redirect to the show page" do 
+        response.should redirect_to(@book)
+        flash[:notice].should_not be_nil
+      end
+    end
+
     describe "GET /show" do 
-      it "should return json data for one book" do
+      it "should respond with success" do
         get :show, id: @book.id
         expect(response.status).to eq 200
       end
     end
 
     describe "PUT /update" do
-      it "should update the record for one book" do
-        get :update, id: @book.id
+      it "should respond with success" do
+        get :update, id: @book.id, book: {:title => "Hermione Granger and the Goblet of Awesome"}
         expect(response.status).to eq 200
       end
     end
 
-  end
 end
