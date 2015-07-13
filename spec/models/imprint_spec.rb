6b4822511082572027182000f48e7064ce09d4a9
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-07-12 23:52:16 -0500

    Adds basic imprint spec.

--- spec/models/imprint_spec.rb
+++ spec/models/imprint_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Imprint do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+  	@imprint = FactoryGirl.create(:imprint)
+  end
+  describe "responses" do
+  	subject { @imprint }
+  	it { should respond_to(:name) }
+  	it { should respond_to(:publisher_id) }
+  end
 end
