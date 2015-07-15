4374b2484d4c2c35f4b91fd1b3ef3bfe40f1f40f
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-07-14 21:35:41 -0500

    Adds Notification model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712040251) do
+ActiveRecord::Schema.define(version: 20150715023507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20150712040251) do
   create_table "imprints", force: true do |t|
     t.integer  "publisher_id"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notifications", force: true do |t|
+    t.string   "message"
+    t.integer  "book_id"
+    t.string   "type"
+    t.integer  "pm_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
