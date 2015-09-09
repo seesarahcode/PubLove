580ba035f42063a9891f597d313523d41c8ffaa3
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-08 23:53:27 -0500

    Starts helper for calendar date alignment.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,25 @@
 module DashboardHelper
+
+	def calendar_alignment(month) 
+		first = Date.today.beginning_of_month.wday #returns 1-7 for M-Sun
+		case first
+		when 1
+			# add one empty div day
+		when 2
+			# add two empty div days
+		when 3
+			# add three empty div days
+		when 4
+			# add four empty div days
+		when 5
+			# add five empty div days
+		when 6
+			# add six empty div days
+		when 7
+			# add seven empty div days
+		else
+			puts "Uh-oh"
+		end
+	end
+
 end
