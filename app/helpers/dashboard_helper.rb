7b515484052ed16df98ea78ef4b2d2dfcef51e02
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-10 23:07:57 -0500

    Calendar alignment working! Needs spacing adjustments for single digits.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,19 +4,23 @@ module DashboardHelper
 		first = Date.today.beginning_of_month.wday
 		case first
 		when 1
-			content_tag(:p)
+			content_tag(:div, "  ", class: "date")
 		when 2
-			content_tag(:p) + content_tag(:p) # refactor multiples for conciseness
+			content_tag(:div, "  ", class: "date") + content_tag(:div, "  ", class: "date") # refactor multiples for conciseness
 		when 3
-			content_tag(:p) + content_tag(:p) + content_tag(:p)
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date")
 		when 4
-			content_tag(:p) + content_tag(:p) + content_tag(:p) + content_tag(:p)
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date")
 		when 5
-			content_tag(:p) + content_tag(:p) + content_tag(:p) +
-			content_tag(:p) + content_tag(:p)
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date")
 		when 6
-			content_tag(:p) + content_tag(:p) + content_tag(:p) +
-			content_tag(:p) + content_tag(:p) + content_tag(:p)
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
+			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date")
 		else
 			puts "Nada"
 		end
