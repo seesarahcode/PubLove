743823c9cb94d1813dacd11f02e4b655a7a3a178
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-11 22:59:32 -0500

    Fixes blank calendar date spacing.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,23 +4,23 @@ module DashboardHelper
 		first = Date.today.beginning_of_month.wday
 		case first
 		when 1
-			content_tag(:div, "  ", class: "date")
+			content_tag(:div, "  ", class: "date-blank")
 		when 2
-			content_tag(:div, "  ", class: "date") + content_tag(:div, "  ", class: "date") # refactor multiples for conciseness
+			content_tag(:div, "  ", class: "date-blank") + content_tag(:div, "  ", class: "date-blank") # refactor multiples for conciseness
 		when 3
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date")
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank")
 		when 4
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date")
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank")
 		when 5
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date")
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank")
 		when 6
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date") + 
-			content_tag(:div, "", class: "date") + content_tag(:div, "", class: "date")
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank") + 
+			content_tag(:div, "", class: "date-blank") + content_tag(:div, "", class: "date-blank")
 		else
 			puts "Nada"
 		end
