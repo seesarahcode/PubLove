098cf4a113b6c3f5e7bb3216d3a2c205316770f8
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-17 19:35:40 -0500

    Finishes publisher step of sign up, onto payments.

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -8,4 +8,16 @@ class UserStepsController < ApplicationController
 		@publisher = @user.publisher
 		render_wizard
 	end
+
+	def update
+    @user = current_user
+    @publisher = @user.publisher
+    @publisher.update_attributes(pub_params)
+    render_wizard @user
+  end
+
+  private
+  def pub_params
+    params.require(:publisher).permit(:id, :admin_id, :name, :street, :city, :state, :zip, :phone, :website)
+  end
 end
