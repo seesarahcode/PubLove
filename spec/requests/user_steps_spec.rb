f3ffc46e5e4e19e01f1b4564313ed4f508573daa
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-09-29 21:59:08 -0500

    Testing through publisher page of signup.

--- spec/requests/user_steps_spec.rb
+++ spec/requests/user_steps_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Admin/account sign up" do
-	context "with valid information" do
+	context "on publisher page" do
 		before do
 			visit new_user_registration_path
 	    fill_in "Email",                 :with => "mrsimon@schuster.com"
@@ -9,15 +9,25 @@ describe "Admin/account sign up" do
 	    fill_in "Password confirmation", :with => "$uperscript"
 	    click_button "Sign up"
 	  end
-	  it "should send the user to the publisher page" do
-	  	pending
-	  end
 	  it "should allow the user to submit publisher information" do
-	  	pending
+	  	fill_in "Name",			:with => "Simon & Schuster"
+	  	fill_in "Phone",		:with => "1-555-888-1010"
+	  	fill_in "Website",	:with => "www.simonandschuster.com"
+	  	click_button "Continue"
 	  end
-	  it "should send the user to the payment page" do
-	  	pending
-	  end
+	end
+	context "on payment page" do
+		before do
+			visit new_user_registration_path
+	    fill_in "Email",                 :with => "mrsimon@schuster.com"
+	    fill_in "Password",              :with => "$uperscript"
+	    fill_in "Password confirmation", :with => "$uperscript"
+	    click_button "Sign up"
+	    click_button "Continue"
+		end
+		it "should allow the user to submit a publisher payment" do
+			pending
+		end
 	end
 	context "with invalid information" do
 	end
