170f1115b78a46ccb79c2cc638e7be1429492486
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-10-01 22:31:29 -0500

    Tests passing for dashboard helper, but don't seem to be catching coverage...hmmm...

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -4,27 +4,45 @@ describe "DashboardHelper" do
 	describe "#calendar_alignment" do
 		it "should return one div if the 1st begins on a Monday" do
 			Time.stub(:now) { Time.new(2015,6,1, 12,00,0, "+09:00").utc }
-			helper.calendar_alignment.should have_selector("div.month-days") do |content|
+			helper.calendar_alignment do |content|
     		content.should have_selector(:div, :class => "date-blank")
     	end
 		end
 		it "should return two divs if the 1st begins on a Tuesday" do
-			pending
+			Time.stub(:now) { Time.new(2015,9,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+    		content.should match("<div class='date-blank'><div class='date-blank'>")
+    	end
 		end
 		it "should return three divs if the 1st begins on a Wednesday" do
-			pending
+			Time.stub(:now) { Time.new(2015,7,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+    		content.should match("<div class='date-blank'><div class='date-blank'><div class='date-blank'>")
+    	end
 		end
 		it "should return four divs if the 1st begins on a Thursday" do
-			pending
+			Time.stub(:now) { Time.new(2015,10,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+    		content.should match("<div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'>")
+    	end
 		end
 		it "should return five divs if the 1st begins on a Friday" do
-			pending
+			Time.stub(:now) { Time.new(2015,5,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+    		content.should match("<div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'>")
+    	end
 		end
 		it "should return six divs if the 1st begins on a Saturday" do
-			pending
+			Time.stub(:now) { Time.new(2015,3,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+    		content.should match("<div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'><div class='date-blank'>")
+    	end
 		end
 		it "should return no divs if the 1st begins on a Sunday" do
-			pending
+			Time.stub(:now) { Time.new(2015,11,1, 12,00,0, "+09:00").utc }
+			helper.calendar_alignment do |content|
+				content.should match("")
+			end
 		end
 	end
 end
