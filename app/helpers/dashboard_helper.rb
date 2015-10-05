45803ca73ce9052eba4a4463120797736deafffa
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-10-04 20:33:13 -0500

    Refactors helper tests, partially fixes return value for helper.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,7 +2,7 @@ module DashboardHelper
 
 	def calendar_alignment 
 		number = Date.today.beginning_of_month.wday
-		number.times.collect {content_tag(:div, "", class: "date-blank") }.join("") unless number == 7
+		number.times.inject {content_tag(:div, "", class: "date-blank") } unless number == 7
 	end
 
 end
--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -8,23 +8,23 @@ describe "DashboardHelper" do
 		end
 		it "should return two divs if the 1st begins on a Tuesday" do
 			Date.stub(:today) { Date.new(2015,9,1) }
-			helper.calendar_alignment.should match("<div class=\"date-blank\"></div><div class=\"date-blank\"></div>")
+			helper.calendar_alignment.should match("<div class=\"date-blank\"></div>" * 2)
 		end
 		it "should return three divs if the 1st begins on a Wednesday" do
 			Date.stub(:today) { Date.new(2015,7,1) }
-			helper.calendar_alignment.should match("<div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div>")
+			helper.calendar_alignment.should match("<div class=\"date-blank\"></div>" * 3)
 		end
 		it "should return four divs if the 1st begins on a Thursday" do
 			Date.stub(:today) { Date.new(2015,10,1) }
-			helper.calendar_alignment.should match("<div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div>")
+			helper.calendar_alignment.should match("<div class=\"date-blank\"></div>" * 4)
 		end
 		it "should return five divs if the 1st begins on a Friday" do
 			Date.stub(:today) { Date.new(2015,5,1) }
-			helper.calendar_alignment.should match("<div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div>")
+			helper.calendar_alignment.should match("<div class=\"date-blank\"></div>" * 5)
 		end
 		it "should return six divs if the 1st begins on a Saturday" do
 			Date.stub(:today) { Date.new(2015,8,1) }
-			helper.calendar_alignment.should match("<div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div><div class=\"date-blank\"></div>")
+			helper.calendar_alignment.should match("<div class=\"date-blank\"></div>" * 6)
 		end
 		it "should return no divs if the 1st begins on a Sunday" do
 			Date.stub(:today) { Date.new(2015,3,1) }
